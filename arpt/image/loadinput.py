f25fb6b9f16f83952d66f0bedaeb7d03d4ee6295
Kenza Arraki <karraki@gmail.com>
2015-01-29 23:29:07 -0700

    converted to python, still functions called that need to be changed

--- arpt/image/loadinput.py
+++ arpt/image/loadinput.py
@@ -1,124 +1,104 @@
-pro loadinput,input0,list,comment=comment,count=count,stp=stp
+"""
+ NAME:
+  LOADINPUT
 
-;+
-;
-; NAME:
-;  LOADINPUT
-;
-; PURPOSE:
-;  This program can be used to load command-line inputs.
-;  The input can be:
-;   (1) an array list of files, i.e. ['one.txt','two.txt']
-;   (2) a globbed list, i.e. "*.txt"
-;   (3) a comma separated list, i.e.  'one.txt,two.txt'
-;   (4) the name of a file that contains a list, i.e. "list.txt"
-;         This can NOT be used in combination with any of the other three options.
-;
-; INPUTS:
-;  input    The input list.  There are three possibilities
-;            (1) an array list of files
-;            (2) a globbed list, i.e. "*.txt"
-;            (3) a comma separated list, i.e.  'one.txt,two.txt'
-;            (4) the name of a file that contains a list, i.e. "@list.txt"
-;                  This can NOT be used in combination with any of the other three options.
-;  =comment Comment string to use when loading a list file. By default comment='#'
-;  /stp     Stop at the end of the program
-;
-; OUTPUTS:
-;  list     The list of files
-;  =count   The number of elements in list
-;
-; USAGE:
-;  IDL>loadinput,'*.fits',list,count=count
-;
-; By D.Nidever   April 2007
-;-
+ PURPOSE:
+  This program can be used to load command-line inputs.
+  The input can be:
+   (1) an array list of files, i.e. ['one.txt','two.txt']
+   (2) a globbed list, i.e. "*.txt"
+   (3) a comma separated list, i.e.  'one.txt,two.txt'
+   (4) the name of a file that contains a list, i.e. "list.txt"
+         This can NOT be used in combination with any of the other three options.
 
-undefine,list
-count = 0
+ INPUTS:
+  input    The input list.  There are three possibilities
+            (1) an array list of files
+            (2) a globbed list, i.e. "*.txt"
+            (3) a comma separated list, i.e.  'one.txt,two.txt'
+            (4) the name of a file that contains a list, i.e. "@list.txt"
+                  This can NOT be used in combination with any of the other three options.
+  =comment Comment string to use when loading a list file. By default comment='#'
+  /stp     Stop at the end of the program
 
-; Not enough inputs
-if n_elements(input0) eq 0 then begin
-  print,'Syntax - loadinput,input,list,comment=comment,count=count,stp=stp'
-  return
-endif
-input = input0
+ OUTPUTS:
+  list     The list of files
+  =count   The number of elements in list
 
-; Empty string input
-if strtrim(input[0],2) eq '' then begin
-  undefine,list
-  count = 0
-  return
-endif
+ USAGE:
+  loadinput('*.fits', list, count=count)
 
+ By D.Nidever   April 2007
+-
+"""
+def loadinput(input0, comment='#', stp=False):
 
-; A list file was input
-if strmid(input[0],0,1) eq '@' then begin
+# KENZA Q: undefine,list -> is None close enough?
+    list = None
+    count = 0
 
-  inp = strmid(input[0],1)
+    # Not enough inputs
+    if len(input0)==0:
+        print 'Syntax - loadinput(input, comment=comment, stp=stp)'
+        return list, count
+    input = input0
 
-  ; Loading the files
-  ;readcol,inp,list,format='A',/silent,comment='#'
-  if n_elements(comment) eq 0 then comment='#'
-  readline,inp,list,comment=comment,/noblank
-  nlist = n_elements(list)
+    # Empty string input
+    if input[0].split()=='':
+        list = None
+        count = 0
+        return list, count
 
-endif else begin
+    # A list file was input
+    if input[0][0:1]=='@':
+        inp = input[0][1:]
+        # Loading the files
+        readline(inp, list, comment=comment, noblank=True)
+        nlist = len(list)
+    else:
+        # Break up comma lists
+        ninput = len(input)
+        temp = None
 
-  ; Break up comma lists
-  ninput = n_elements(input)
-  undefine,temp
-  for i=0.,ninput-1 do begin
-    dum = strsplit(input[i],',',/extract)
-    push,temp,dum
-  end
-  input = temp
-  undefine,temp
-  if n_elements(input) eq 1 then input=input[0]
+        for i in range(0,ninput):
+            dum = input[i].split(',')
+            push(temp,dum)
 
-  ; Probably an array of filenames
-  if n_elements(input) gt 1 then begin
+        input = temp
+        temp = None
+        if len(input)==1: input=input[0]
 
-    ; Try to expand as wildcards
-    wildind = where(strpos(input,'*') ne -1 or strpos(input,'?') ne -1,nwild)
-    normind = where(strpos(input,'*') eq -1 and strpos(input,'?') eq -1,nnorm)
-    
-    ; Some wildcards
-    if (nwild gt 0) then begin
+        # Probably an array of filenames
+        if len(input)>1:
+            # Try to expand as wildcards
+            wildind = where(strpos(input,'*')!=-1 or strpos(input,'?')!=-1, nwild)
+            normind = where(strpos(input,'*')!=-1 and strpos(input,'?')!=-1,nnorm)
 
-      undefine,list
-      if nnorm gt 0 then push,list,input[normind]
-      wild = input[wildind]
+            # Some wildcards
+            if nwild>0:
+                list = None
+                if nnorm>0: push(list,input[normind])
+                wild = input[wildind]
 
-      ; Loop through the wildcards
-      for i=0.,nwild-1 do begin
-        wfiles = file_search(wild[i],count=nwfiles)
-        if nwfiles gt 0 then push,list,wfiles
-      end
+                # Loop through the wildcards
+                for i in range(0,nwild):
+                    wfiles = file_search(wild[i],count=nwfiles)
+                    if nwfiles>0: push(list,wfiles)
       
-      nlist = n_elements(list)
+                nlist = len(list)
 
-    ; No wildcards
-    endif else begin
+            # No wildcards
+            else:
+                list = input
+                nlist = len(list)
+            
+        # A globbed list or only one file
+        else:
+            list = file_search(input, count=nlist)
+            if nlist==0: list=input    # Maybe just one filename
+            #nlist = len(list)
 
-      list = input
-      nlist = n_elements(list)
-
-    endelse
-
-
-  ; A globbed list or only one file
-  endif else begin
-    list = file_search(input,count=nlist)
-    if nlist eq 0 then list=input    ; Maybe just one filename
-    ;nlist = n_elements(list)
-  endelse
-
-endelse
-
-count = n_elements(list)
-if count eq 1 then list=list[0]
-
-if keyword_set(stp) then stop
-
-end
+    count = len(list)
+    if count==1: list=list[0]
+    if stp: exit()
+    return list, count
