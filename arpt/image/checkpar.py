ffc5cfb5c350c7ade45023b8ef1de57f933643ec
Kenza Arraki <karraki@gmail.com>
2015-02-03 21:58:07 -0700

    basic changes to syntax, need to figure out case of and type

--- arpt/image/checkpar.py
+++ arpt/image/checkpar.py
@@ -1,106 +1,107 @@
-;+
-; NAME:
-;     CHECKPAR
-; PURPOSE: (one line)
-;     Validate an input parameter against valid entries.
-; DESCRIPTION:
-;
-;     This is a general parameter checking function for validating input
-;     quantities in other procedures and functions.  This routine will
-;     generate an error message indicating what is wrong with the item.
-;
-;     Example of use:
-;
-;     pro foo,array
-;     if checkpar(array,[4,5],2,CALLER='foo') then return
-;        .
-;        . code for foo .
-;        .
-;     end
-;
-;
-;     This would cause an immediate return to the routine that called foo
-;     with an error message if the input was not either floating or double
-;     and 2 dimensional.
-;
-;     As of IDL v3.0, these are the recognized type codes (see 1-218 in
-;        reference guide).
-;
-;        Type
-;        Code     Data Type
-;        ----    -----------------------------
-;          0      Undefined
-;          1      Byte
-;          2      Integer
-;          3      Longword integer
-;          4      Floating point
-;          5      Double-precision floating
-;          6      Complex floating
-;          7      String
-;          8      Structure
-;          9      Double-precision complex
-;         10      Pointer
-;         11      Object reference
-;         12      Unsigned integer
-;         13      Unsigned Longword integer
-;         14      64-bit integer
-;         15      Unsigned 64-bit integer
-;
-; CATEGORY:
-;  Utility
-; CALLING SEQUENCE:
-;     val = checkpar(param,goodtype,goodrank)
-; INPUTS:
-;     param    - IDL variable to validate.
-;     goodtype - Scalar or vector of type codes that are valid.
-;     goodrank - Scalar or vector of valid ranks.
-; OPTIONAL INPUT PARAMETERS:
-; KEYWORD PARAMETERS:
-;     CALLER   - String identifying the calling routine.
-;     DEFAULT  - Value to return in param if undefined and undefined allowed.
-;     DIMEN    - Dimensions of variable.
-;     NPTS     - Total number of elements in variable.
-;     RANK     - Rank of variable.
-;     TYPE     - Type of variable.
-;     /SILENT  - Don't print anything to the screen
-; OUTPUTS:
-;     Return value is 1/TRUE if the parameter is BAD.  0/False if good.
-;     =ERRSTR  - The error string if the parameter is bad.  An empty
-;                 string is returned if the parameter is okay/good.
-; COMMON BLOCKS:
-; SIDE EFFECTS:
-; RESTRICTIONS:
-; PROCEDURE:
-; MODIFICATION HISTORY:
-;     3/24/93 - Written by Marc W. Buie, Lowell Observatory.
-;     4/27/93 - MWB, added TYPE and DEFAULT keywords.
-;     2000/11/1, MWB, added new types for IDL v5.4
-;     2014/14/4, D. Nidever, renamed to checkpar.pro and made some modifications
-;-
-function checkpar,param,goodtype,goodrank, $
-            CALLER=caller, DEFAULT=default, DIMEN=dimen, $
-            NPTS=npts, RANK=rank, TYPE=type, silent=silent, errstr=errstr
+"""
++
+ NAME:
+     CHECKPAR
+ PURPOSE: (one line)
+     Validate an input parameter against valid entries.
+ DESCRIPTION:
+
+     This is a general parameter checking function for validating input
+     quantities in other procedures and functions.  This routine will
+     generate an error message indicating what is wrong with the item.
+
+     Example of use:
+
+     pro foo,array
+     if checkpar(array,[4,5],2,CALLER='foo') then return
+        .
+        . code for foo .
+        .
+     end
+
+
+     This would cause an immediate return to the routine that called foo
+     with an error message if the input was not either floating or double
+     and 2 dimensional.
+
+     As of IDL v3.0, these are the recognized type codes (see 1-218 in
+        reference guide).
+
+        Type
+        Code     Data Type
+        ----    -----------------------------
+          0      Undefined
+          1      Byte
+          2      Integer
+          3      Longword integer
+          4      Floating point
+          5      Double-precision floating
+          6      Complex floating
+          7      String
+          8      Structure
+          9      Double-precision complex
+         10      Pointer
+         11      Object reference
+         12      Unsigned integer
+         13      Unsigned Longword integer
+         14      64-bit integer
+         15      Unsigned 64-bit integer
+
+ CATEGORY:
+  Utility
+ CALLING SEQUENCE:
+     val = checkpar(param,goodtype,goodrank)
+ INPUTS:
+     param    - IDL variable to validate.
+     goodtype - Scalar or vector of type codes that are valid.
+     goodrank - Scalar or vector of valid ranks.
+ OPTIONAL INPUT PARAMETERS:
+ KEYWORD PARAMETERS:
+     CALLER   - String identifying the calling routine.
+     DEFAULT  - Value to return in param if undefined and undefined allowed.
+     DIMEN    - Dimensions of variable.
+     NPTS     - Total number of elements in variable.
+     RANK     - Rank of variable.
+     TYPE     - Type of variable.
+     /SILENT  - Don't print anything to the screen
+ OUTPUTS:
+     Return value is 1/TRUE if the parameter is BAD.  0/False if good.
+     =ERRSTR  - The error string if the parameter is bad.  An empty
+                 string is returned if the parameter is okay/good.
+ COMMON BLOCKS:
+ SIDE EFFECTS:
+ RESTRICTIONS:
+ PROCEDURE:
+ MODIFICATION HISTORY:
+     3/24/93 - Written by Marc W. Buie, Lowell Observatory.
+     4/27/93 - MWB, added TYPE and DEFAULT keywords.
+     2000/11/1, MWB, added new types for IDL v5.4
+     2014/14/4, D. Nidever, renamed to checkpar.pro and made some modifications
+-
+"""
+
+def checkpar(param, goodtype, goodrank, caller=caller, default=default, dimen=dimen, npts=npts, rank=rank, type=type, silent=True, errstr='')
 
    errval = 0
-   errstr = ''   ; initialize error string
 
-   ; Not enough inputs
-   if n_elements(goodtype) eq 0 then begin
-     print,'Syntax - error = checkpar(param,goodtype,goodrank, CALLER=caller, DEFAULT=default,'
-     print,'                          DIMEN=dimen, NPTS=npts, RANK=rank, TYPE=type, silent=silent'
-     return,'Not Enough Inputs'
-   endif
+   # Not enough inputs
+   if goodtype==0:
+     print 'Syntax - error = checkpar(param,goodtype,goodrank, CALLER=caller, DEFAULT=default, DIMEN=dimen, NPTS=npts, RANK=rank, TYPE=type, silent=silent'
+     return 'Not Enough Inputs'
 
    sz = size(param)
    rank = sz[0]
    type = sz[rank+1]
    npts = sz[rank+2]
-   if rank eq 0 then dimen=0 else dimen = sz[1:rank]
+   if rank==0:
+       dimen=0
+   else:
+       dimen = sz[1:rank]
 
-   ; Check variable type
-   z=where(goodtype eq type, count)
-   if count eq 0 then begin
-      case type of
+   # Check variable type
+   z=where(goodtype==type, count)
+   if count==0:
+#      case type of
          0 :    err1 = 'Undefined variable is not allowed.'
          1 :    err1 = 'Byte variable type is not allowed.'
          2 :    err1 = 'Integer variable type is not allowed.'
@@ -117,17 +118,17 @@ function checkpar,param,goodtype,goodrank, $
          13:    err1 = 'Unsigned longword integer type is not allowed.'
          14:    err1 = '64-bit integer type is not allowed.'
          15:    err1 = 'Unsigned 64-bit integer type is not allowed.'
-         else : err1 = 'Unrecognized variable type code.  Impossible!'
-      endcase
+         else:
+             err1 = 'Unrecognized variable type code.  Impossible!'
+#      endcase
       errval = 0
       errstr += err1
-   endif
 
-   ; Check variable rank
-   if type ne 0 and n_elements(goodrank) gt 0 then begin
-      z=where(goodrank eq rank, count)
-      if count eq 0 then begin
-         case rank of
+   # Check variable rank
+   if type!=0 and goodrank>0:
+      z=where(goodrank==rank, count)
+      if count==0:
+#         case rank of
             0 :    err2 = 'Scalar variables are not allowed.'
             1 :    err2 = 'Vector variables are not allowed.'
             2 :    err2 = '2-D variables are not allowed.'
@@ -137,33 +138,28 @@ function checkpar,param,goodtype,goodrank, $
             6 :    err2 = '6-D variables are not allowed.'
             7 :    err2 = '7-D variables are not allowed.'
             8 :    err2 = '8-D variables are not allowed.'
-            else : err2 = 'Unrecognized variable rank.  Impossible!'
-         endcase
+            else:
+                err2 = 'Unrecognized variable rank.  Impossible!'
+#         endcase
          errval = 1
-         if errstr ne '' then errstr+=' '
+         if errstr!='':
+             errstr += ' '
          errstr += err2
-      endif
-   endif
 
-   ; There was an error
-   if errval ne 0 then begin
-      if n_elements(caller) gt 0 then errstr = caller+errstr
-      if not keyword_set(silent) then print,errstr
-      return,errval
-   endif
+   # There was an error
+   if errval!=0:
+      if caller>0: errstr = caller + errstr
+      if not silent: print errstr
+      return errval
 
-   ; Default value if undefined input and undefined allowed
-   if type eq 0 then begin
+   # Default value if undefined input and undefined allowed
+   if type==0:
       szd = size(default)
-      if szd[szd[0]+1] ne 0 then begin
+      if szd[szd[0]+1]!=0:
          param = default
-         sz    = size(param)
-         rank  = sz[0]
-         type  = sz[rank+1]
-         npts  = sz[rank+2]
-      endif
-   endif
+         sz = size(param)
+         rank = sz[0]
+         type = sz[rank+1]
+         npts = sz[rank+2]
 
-   return,errval
-
-end
+   return errval
